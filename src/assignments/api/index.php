920a692b6161ad7be91f8f7509fa80d5732e3093
devgirl-code <mubeenabbasi2009@gmail.com>
2025-12-06 12:49:04 +0300

    added session cookies

--- src/assignments/api/index.php
+++ src/assignments/api/index.php
@@ -1,4 +1,14 @@
 <?php
+session_set_cookie_params([
+    'lifetime' => 0,
+    'path' => '/',
+    'httponly' => true,
+    'samesite' => 'None',
+    'secure' => false
+]);
+require_once '../../auth/api/auth_check.php';
+requireRole('admin'); 
+
 /**
  * Assignment Management API
  * RESTful API for assignments and comments
